ce0483ab96effb41b1b86dafe58737719977f1b2
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-12-21 18:11:16 +0000

    Add note to transformer tests

--- alan_transformer/tests/test_transformer.py
+++ alan_transformer/tests/test_transformer.py
@@ -4,6 +4,13 @@ from alan_transformer.transformer import Transformer
 
 
 class TestTransformer:
+    """Transformer tests
+
+    Unlike the constituent parts, testing the full transformer is challenging as
+    there are a lot of calculations in each layer. As such we'll just use a
+    snapshot test here.
+    """
+
     def test_architecture_snapshot(self, mocker, snapshot):
         # Mock the weight random initialisation (use ones instead)
         mocker.patch("torch.rand", new=torch.ones)
