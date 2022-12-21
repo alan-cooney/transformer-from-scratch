1176cfb27154b178da71bc48aeed88bac0232c0d
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-12-21 11:24:34 +0000

    Add positional encoding functionality

---
+++ alan_transformer/tests/test_positional_encoding.py
@@ -0,0 +1,27 @@
+import math
+
+import torch
+from torchtyping import TensorType as TT
+
+from alan_transformer.positional_encoding import positional_encoding
+
+
+def test_pos_encoding_2_token():
+    """Test the pos=2 token"""
+    # Set the embedding to zeros
+    d_model: int = 4
+    embedding: TT["batch", "pos", "d_model"] = torch.zeros(1, 4, d_model, 2)
+    
+    # Create the expected positional encoding
+    position: int = 2
+    expected_positional_encoding = []
+    for dim in range(d_model):
+        if dim %2 == 0:
+            pos_embed = math.sin(position / (10000 ** (dim/d_model)))
+        else:
+            pos_embed = math.cos(position / (10000 ** ((dim - 1)/d_model)))
+        expected_positional_encoding.append(pos_embed)
+        
+    # Compare against the res
+    encoding: TT["batch", "pos", "d_model"] = positional_encoding(embedding)
+    assert torch.allclose(encoding[0, position, :], torch.tensor(expected_positional_encoding))
