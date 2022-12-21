4d1f491ad9d5126bf4f7ebf8f19eacd9278f3f3c
Alan <41682961+alan-cooney@users.noreply.github.com>
2022-12-21 15:42:08 +0000

    Add the full attention layer

---
+++ alan_transformer/attention.py
@@ -0,0 +1,108 @@
+from torch import nn
+from torchtyping import TensorType as TT
+from einops import rearrange
+import math
+import torch
+from fancy_einsum import einsum
+
+QueryType = TT["batch", "head", "dest", "d_head"]
+KeyType = TT["batch", "head", "src", "d_head"]
+ValueType = TT["batch", "head", "src", "d_head"]
+QKVWeightType = TT["head", "d_model", "d_head"]
+AttentionPatternType = TT["batch", "head", "dest", "src"]
+AttentionOutputType = TT["batch", "head", "pos", "d_head"]
+ResidualStreamType = TT["batch", "pos", "d_model"]
+
+
+class AttentionLayer(nn.Module):
+    """Attention Layer"""
+
+    def __init__(self, d_head: int = 64, d_model: int = 768) -> None:
+        """Create the attention layer"""
+        super().__init__()
+        # Check the params
+        assert d_model % d_head == 0, "d_model must be a multiple of d_head"
+
+        # Set number of heads
+        n_heads: int = int(d_model / d_head)
+
+        # Store d_head sqrt for the attention calculation
+        self.d_head_sqrt: float = math.sqrt(d_head)
+        # Create the parameters
+        self.weight_query: QKVWeightType = nn.Parameter(
+            torch.rand(n_heads, d_model, d_head))
+        self.weight_key: QKVWeightType = nn.Parameter(
+            torch.rand(n_heads, d_model, d_head))
+        self.weight_value: QKVWeightType = nn.Parameter(
+            torch.rand(n_heads, d_model, d_head))
+        self.weight_out: TT["d_model", "d_model"] = nn.Parameter(
+            torch.rand(d_model, d_model))
+
+    def mask(self, attention_pattern: AttentionPatternType) -> AttentionPatternType:
+        """Mask the attention pattern"""
+        return torch.tril(attention_pattern)
+
+    def attention(self, query: QueryType, key: KeyType, value: ValueType) -> AttentionOutputType:
+        """Attention Calculation
+
+        Attention(Q,K,V) = softmax( (Q K^T) / (sqrt(d_head)) ) * V
+
+        https://arxiv.org/pdf/1706.03762.pdf (p4)
+
+        Args:
+            query (QueryType): Query
+            key (KeyType): Key
+            value (ValueType): Value
+
+        Returns:
+            AttentionOutputType: Attention Output (softmax * value)
+        """
+        # Calculate the numerator
+        key_transpose: TT["batch", "head", "d_head", "pos"] = rearrange(
+            key,
+            "batch head pos d_head -> batch head d_head pos"
+        )
+        numerator: AttentionPatternType = query @ key_transpose
+
+        # Apply softmax over the attention pattern
+        attention_pattern: AttentionPatternType = numerator / self.d_head_sqrt
+        masked_attention: AttentionPatternType = self.mask(
+            attention_pattern)
+        softmax_part: AttentionPatternType = torch.softmax(
+            masked_attention,
+            dim=-1  # Apply over the last (src) dimension
+        )
+
+        return einsum(
+            "batch head dest src, batch head src d_head -> batch head dest d_head",
+            softmax_part,
+            value
+        )
+
+    def forward(self, residual_stream: ResidualStreamType) -> ResidualStreamType:
+        """Attention layer forward pass
+
+        https://arxiv.org/pdf/1706.03762.pdf (p5)
+        """
+        # Create the query, key and value
+        query: QueryType = einsum("batch pos d_model, head d_model d_head -> batch head pos d_head",
+                                  residual_stream, self.weight_query)
+        key: KeyType = einsum("batch pos d_model, head d_model d_head -> batch head pos d_head",
+                              residual_stream, self.weight_key)
+        value: ValueType = einsum("batch pos d_model, head d_model d_head -> batch head pos d_head",
+                                  residual_stream, self.weight_value)
+
+        # Get the attention & concat
+        attn: AttentionOutputType = self.attention(query, key, value)
+        attn_concat: ResidualStreamType = rearrange(
+            attn,
+            "batch head pos d_head -> batch pos (head d_head)"
+        )
+
+        # Multiply by W_O
+        multi_head_out: ResidualStreamType = einsum(
+            "batch pos d_model, d_model d_model -> batch pos d_model",
+            attn_concat, self.weight_out)
+
+        # Return the attention output plus the residual stream
+        return multi_head_out + residual_stream
---
+++ alan_transformer/tests/test_attention.py
@@ -0,0 +1,88 @@
+import math
+
+import torch
+
+from alan_transformer.attention import (AttentionLayer, AttentionOutputType,
+                                        AttentionPatternType, KeyType,
+                                        QueryType, ValueType, ResidualStreamType)
+
+
+class TestAttention:
+    def test_mask(self):
+        """Test that it masks correctly"""
+        attention_pattern: AttentionPatternType = torch.tensor(
+            [[1., 1], [1, 1]]).unsqueeze(0).unsqueeze(0)
+        expected: AttentionPatternType = torch.tensor(
+            [[1., 0], [1, 1]]).unsqueeze(0).unsqueeze(0)
+
+        attention_layer = AttentionLayer(d_head=2, d_model=4)
+        res = attention_layer.mask(attention_pattern)
+        assert torch.allclose(res, expected)
+
+    def test_attention_simple(self):
+        """Test a simple attention calculation"""
+        # Create the query, key and value
+        query: QueryType = torch.tensor(
+            [[1., 2], [3, 4]]).unsqueeze(0).unsqueeze(0)
+        key: KeyType = torch.tensor(
+            [[5., 6], [7, 8]]).unsqueeze(0).unsqueeze(0)
+        value: ValueType = torch.tensor(
+            [[9., 10], [11, 12]]).unsqueeze(0).unsqueeze(0)
+
+        # Create the expected output
+        numerator = query @ key.transpose(-2, -1)
+        denominator: float = float(math.sqrt(2))
+        masked_attention_pattern = torch.tril(numerator / denominator)
+        expected = torch.softmax(masked_attention_pattern, dim=-1) @ value
+
+        # Create the attention layer
+        attention_layer = AttentionLayer(d_head=2, d_model=4)
+
+        # Calculate the output
+        output: AttentionOutputType = attention_layer.attention(
+            query, key, value)
+
+        # Check the output
+        assert torch.allclose(output, expected)
+
+    def test_attention_artificial(self):
+        """Test an artificial input where all destination tokens attend to the
+        first src token"""
+        # Create the query, key and value
+        # Always attend to the first token
+        query: QueryType = torch.tensor(
+            [[1., 1], [1, 1]]).unsqueeze(0).unsqueeze(0)
+        key: KeyType = torch.tensor(
+            [[999., 999], [-999, -999]]).unsqueeze(0).unsqueeze(0)
+        value: ValueType = torch.tensor(
+            [[3., 3], [-3, -3]]).unsqueeze(0).unsqueeze(0)
+
+        # Create the expected output
+        expected: AttentionOutputType = torch.tensor(
+            [[3., 3], [3, 3]]).unsqueeze(0).unsqueeze(0)
+
+        # Create the attention layer
+        attention_layer = AttentionLayer(d_head=2, d_model=4)
+
+        # Calculate the output
+        output: AttentionOutputType = attention_layer.attention(
+            query, key, value)
+
+        # Check the output
+        assert torch.allclose(output, expected)
+
+    def test_forward_mock_weights_ones(self, mocker):
+        mocker.patch("torch.rand", new=torch.ones)
+
+        # Create a mock residual stream (that sums to 1)
+        d_head: int = 64
+        d_model: int = 768
+        input: ResidualStreamType = torch.ones(
+            1, 10, d_model) / d_model
+
+        # Get the output
+        attention_layer = AttentionLayer(d_head, d_model)
+        res = attention_layer(input)
+
+        # Expect the res (minus 1 for the values) to be the same as the input
+        assert torch.allclose(res - 1, input, atol=1e4)
