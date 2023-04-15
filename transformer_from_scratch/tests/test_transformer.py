1048b973e602a7a021aee06edaebcb9385b9ea87
Alan <41682961+alan-cooney@users.noreply.github.com>
2023-04-15 22:21:27 +0000

    Add the full transformer

---
+++ transformer_from_scratch/tests/test_transformer.py
@@ -0,0 +1,104 @@
+"""Transformer Tests."""
+import random
+
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset, random_split
+
+from transformer_from_scratch.train import cross_entropy_loss
+from transformer_from_scratch.transformer import Transformer
+from transformer_from_scratch.types import (
+    BatchLogitsTT,
+    TokenIndicesTT,
+)
+
+
+class InductionHeadDataset(Dataset):
+    """Induction Head Dataset.
+
+    One thing that 2-layer (and above) transformers can do is to learn to repeat a pattern. This
+    dataset therefore contains a sequence of tokens, where the first few tokens are a random pattern
+    and then the rest of the sequence is the pattern repeated (potentially more than one time).
+    """
+
+    samples: list[TokenIndicesTT]
+
+    def __init__(self, num_samples: int, sequence_length: int, d_vocab: int):
+        self.num_samples = num_samples
+        self.sequence_length = sequence_length
+        self.d_vocab = d_vocab
+
+    def __len__(self) -> int:
+        return self.num_samples
+
+    def __getitem__(self, _index) -> TokenIndicesTT:
+        initial_pattern_length = random.randint(2, self.sequence_length - 2)
+
+        sample: list[int] = []
+        for token_idx in range(self.sequence_length):
+            # Create the initial pattern first
+            if token_idx < initial_pattern_length:
+                token = random.randint(1, self.d_vocab - 1)
+                sample.append(token)
+
+            # If we've reached the end of the initial pattern, then repeat it until the end of
+            # the sequence.
+            else:
+                token = sample[token_idx % initial_pattern_length]
+                sample.append(token)
+
+        sample_tensor: TokenIndicesTT = torch.tensor(sample, dtype=torch.long)
+        return sample_tensor
+
+
+def test_transformer_induction_heads():
+    """Test that the transformer can learn to repeat a pattern."""
+    d_vocab = 50000
+    num_samples = 10000
+    sequence_length = 20
+    epochs = 1
+    model = Transformer(d_vocab=d_vocab, n_layers=2)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    dataset = InductionHeadDataset(num_samples, sequence_length, d_vocab)
+    train_size = int(len(dataset) * 0.95)
+    test_size = len(dataset) - train_size
+    train_data, test_data = random_split(dataset, [train_size, test_size])
+    train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=32)
+
+    # Train the model
+    for _epoch in range(epochs):
+        model.train()
+        for inputs in train_loader:
+            optimizer.zero_grad()
+            logits: BatchLogitsTT = model(inputs)
+            loss = cross_entropy_loss(inputs, logits)
+            loss.backward()
+            optimizer.step()
+
+    # Test the model
+    model.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs in test_loader:
+            outputs: BatchLogitsTT = model(inputs)
+
+            penultimate_token_logits = outputs[:, -2, :]
+            penultimate_tokens = torch.argmax(penultimate_token_logits, dim=-1)
+            correct_last_tokens = inputs[:, -1]
+
+            for token_idx, correct_last_token in enumerate(correct_last_tokens):
+                predicted = penultimate_tokens[token_idx]
+                if correct_last_token == predicted:
+                    correct += 1
+                total += 1
+
+    accuracy = correct / total
+    assert accuracy > 0.8, f"Expected accuracy > 0.8, but got {accuracy}"
+
+
+def test_transformer_model_architecture_matches_snapshot(snapshot):
+    """Test that the transformer model architecture matches the snapshot."""
+    model = Transformer(d_vocab=10000, n_layers=6)
+    snapshot.assert_match(model)
---
+++ transformer_from_scratch/transformer.py
@@ -0,0 +1,81 @@
+"""Transformer."""
+from torch import nn
+
+from transformer_from_scratch.components.embed_unembed import Embed, Unembed
+from transformer_from_scratch.components.layer import Layer
+from transformer_from_scratch.components.positional_encoding import PositionalEncoding
+from transformer_from_scratch.types import (
+    BatchLogitsTT,
+    BatchResidualStreamTT,
+    BatchTokenIndicesTT,
+)
+
+
+class Transformer(nn.Module):
+    """Encoder Only Transformer."""
+
+    def __init__(
+        self,
+        d_head: int = 64,
+        d_hidden: int = 2048,
+        d_model: int = 768,
+        d_vocab: int = 50432,
+        max_tokens: int = 1024,
+        n_layers: int = 12,
+    ) -> None:
+        """Initialise the Transformer.
+
+        Default args are taken from the Attention is All You Need paper, with the exception of
+        d_vocab which defaults to the GPT-NeoX vocab size.
+
+        Args:
+            d_head (int, optional): Number of head features per token.
+            d_hidden (int, optional): Number of hidden layer features per token.
+            d_model (int): Number of residual stream features per token.
+            d_vocab: Number of tokens in the vocabulary.
+            max_tokens: Maximum number of tokens in a sequence.
+            n_layers: Number of layers (attention + mlp = 1 layer) in the architecture.
+        """
+        super().__init__()
+
+        # Embedding and unembedding
+        self.embed = Embed(d_vocab=d_vocab, d_model=d_model)
+        self.unembed = Unembed(d_vocab=d_vocab, d_model=d_model)
+
+        # Positional encoding
+        self.positional_encoding = PositionalEncoding(
+            d_model=d_model,
+            max_tokens=max_tokens,
+        )
+
+        # Layers
+        self.layers = nn.ModuleList([])
+        for _layer_idx in range(n_layers):
+            self.layers.append(
+                Layer(
+                    d_model=d_model,
+                    d_head=d_head,
+                    d_hidden=d_hidden,
+                    max_tokens=max_tokens,
+                )
+            )
+
+    def forward(self, tokens: BatchTokenIndicesTT) -> BatchLogitsTT:
+        """Forward pass.
+
+        Args:
+            tokens (BatchTokenIndicesTT): Input tokens (indices rather than one-hot)
+
+        Returns:
+            BatchLogitsTT: Logits representing log probabilities for the tokens
+        """
+        # Embed + positional encoding
+        residual_stream: BatchResidualStreamTT = self.embed(tokens)
+        residual_stream = self.positional_encoding(residual_stream)
+
+        # Loop through layers
+        for layer in self.layers:
+            residual_stream = layer(residual_stream)
+
+        # Unembed and return
+        return self.unembed(residual_stream)
