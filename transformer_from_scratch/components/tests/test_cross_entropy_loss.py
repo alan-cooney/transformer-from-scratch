5bf3b7faa746cbe4af7021990110bd760215c977
Alan <41682961+alan-cooney@users.noreply.github.com>
2023-04-25 17:16:43 +0000

    Improve cross entropy loss testing

--- transformer_from_scratch/components/tests/test_cross_entropy_loss.py
+++ transformer_from_scratch/components/tests/test_cross_entropy_loss.py
@@ -22,3 +22,31 @@ def test_where_logits_are_fully_accurate():
     # Check the loss is 0
     loss = cross_entropy_loss(inputs, logits).item()
     assert loss == 0
+
+
+def test_loss_with_random_logits():
+    """Test loss with random logits."""
+    # Create some sample inputs
+    sample_tokens = torch.tensor([[0, 1, 2, 3]])
+    inputs: BatchTokenIndicesTT = sample_tokens[:, :-1]  # 1,2,3
+
+    # Create random logits
+    logits: BatchLogitsTT = torch.randn(1, 3, 4)
+
+    # Check the loss is greater than 0 (since the logits are random)
+    loss = cross_entropy_loss(inputs, logits).item()
+    assert loss > 0
+
+
+def test_loss_with_uniform_logits():
+    """Test loss with uniform logits."""
+    # Create some sample inputs
+    sample_tokens = torch.tensor([[0, 1, 2, 3]])
+    inputs: BatchTokenIndicesTT = sample_tokens[:, :-1]  # 1,2,3
+
+    # Create uniform logits
+    logits: BatchLogitsTT = torch.zeros(1, 3, 4)
+
+    # Check the loss is equal to the negative log of the inverse of the vocabulary size
+    loss = cross_entropy_loss(inputs, logits).item()
+    assert loss == -torch.log(torch.tensor(1 / 4)).item()
